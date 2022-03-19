4ed4584a42fde96ef5482f6b90983acd1efa86bf
xyz <gky44px1999@gmail.com>
2022-03-19 16:52:09 -0700

    change autostart screen numbers

--- config.def.h
+++ config.def.h
@@ -29,9 +29,9 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ NULL,       NULL,       "video0",   0,            0,            0 },
-	{ NULL,       NULL,       "video2",   0,            0,            1 },
-	{ NULL,       NULL,       "video4",   0,            0,            2 },
+	{ NULL,       NULL,       "video0",   0,            0,            1 },
+	{ NULL,       NULL,       "video2",   0,            0,            2 },
+	{ NULL,       NULL,       "video4",   0,            0,            3 },
 	{ NULL,       NULL,       "video6",   1 << 1,       0,            1 },
 };
 
